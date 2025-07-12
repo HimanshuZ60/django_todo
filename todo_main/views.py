6e59c0340701160ceae0d0f83bc5be81b021185f
HimanshuZ60 <himanshuz603050@gmail.com>
2025-07-12 14:37:01 +0530

    Adding buttons functionality

--- todo_main/views.py
+++ todo_main/views.py
@@ -4,7 +4,10 @@ from todo.models import Task
 
 def home(requests):
     tasks = Task.objects.filter(completed = False).order_by('-updated_at')
+
+    completed_task = Task.objects.filter(completed = True)
     context = {
-        'tasks': tasks
+        'tasks': tasks,
+        'completed_task' : completed_task
     }
     return render(requests, 'home.html', context)
